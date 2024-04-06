dd9e0dd23d7fa6a857aeae34ded458a5dfcbfffe
maulibrahim20 <maulibrahim20@student.polindra.ac.id>
2024-04-06 12:25:38 +0700

    add padi and palawija.php

---
+++ app/Models/Operator/Padi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Operator;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Padi extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/Operator/Palawija.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Operator;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Palawija extends Model
+{
+    use HasFactory;
+}
