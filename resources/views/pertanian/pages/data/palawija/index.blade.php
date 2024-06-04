5159a40c747e56a3e090da788166de1df642b6b9
maulanaibrahim20 <maulibrahim20@student.polindra.ac.id>
2024-06-04 11:28:08 +0700

    add laporan palawija, export pdf,filtering data using session and fix auth

---
+++ resources/views/pertanian/pages/data/palawija/index.blade.php
@@ -0,0 +1,172 @@
+@extends('index')
+@section('title', 'Data Palawija')
+@section('content')
+    <div class="page-header d-sm-flex d-block">
+        <ol class="breadcrumb mb-sm-0 mb-3">
+            <!-- breadcrumb -->
+            <li class="breadcrumb-item"><a href="{{ url('/pertanian/dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Laporan Palawija</li>
+        </ol><!-- End breadcrumb -->
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Data Laporan Luas Tanaman Palawija</h3>
+                    <div class="dropdown ms-auto">
+                        @if (empty(session('filtering')))
+                        @else
+                            <form style="display: inline;" action="{{ url('/pertanian/data_palawija/exportPdf') }}"
+                                method="get" target="_blank">
+                                @if (session('filterKecamatanData') && session('filterDesaData'))
+                                    <input type="hidden" name="filterKecamatan"
+                                        value="{{ session('filterKecamatanData') }}">
+                                    <input type="hidden" name="filterDesa" value="{{ session('filterDesaData') }}">
+                                    <input type="hidden" name="filterDate" value="{{ session('filterDateData') }}">
+                                @endif
+                                <button class="btn btn-outline-default fw-bold text-primary" type="submit">
+                                    <i class="fa fa-print fw-semibold"></i> Export PDF
+                                </button>
+                            </form>
+                        @endif
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <form action="{{ url('/pertanian/data_palawija/filter') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <div class="row">
+                                    <label class="form-label col-sm-1" style="margin-top: 5px;"> Filter : </label>
+                                    <div class="col-md-2">
+                                        <select name="filterKecamatan" class="form-control select2 form-select"
+                                            data-placeholder="Pilih Kecamatan" id="filterKecamatan" required>
+                                            <option value="">- Pilih Kecamatan -</option>
+                                            @foreach ($filterKecamatan as $kec)
+                                                <option value="{{ $kec->id }}"
+                                                    @if (session('filtering') && session('filtering')->contains('kecamatan_id', $kec->id)) selected @endif>
+                                                    {{ $kec->name }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <select name="filterDesa" class="form-control select2 form-select"
+                                            data-placeholder="Pilih Desa" id="filterDesa" required>
+                                            <option value="">- Pilih Desa -</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="input-group">
+                                            <div class="input-group-text">
+                                                <i class="typcn typcn-calendar-outline tx-24 lh--9 op-6"></i>
+                                            </div>
+                                            <input type="text" name="dateRange" class="form-control" id="daterange"
+                                                placeholder="Pilih Range Tanggal" value="{{ session('dateRange') }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-1">
+                                        <input type="submit" class="btn btn-pill btn-primary" value="FILTER">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <table class="table table-bordered text-nowrap border-bottom" id="responsive-datatable">
+                            <thead>
+                                <tr>
+                                    <th class="wd-15p border-bottom-0">No</th>
+                                    <th class="wd-15p border-bottom-0">Alamat</th>
+                                    <th class="wd-15p border-bottom-0">Jenis Lahan</th>
+                                    <th class="wd-20p border-bottom-0">Tanggal Input</th>
+                                    <th class="wd-20p border-bottom-0 text-center">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if (empty(session('filtering')))
+                                    @foreach ($laporanPalawija as $data)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>
+                                                <div class="d-flex contact-image">
+                                                    <div class="d-flex mt-1 flex-column ms-2">
+                                                        <h6 class="mb-0 fs-14 fw-semibold text-dark">Kecamatan : <span
+                                                                class="badge bg-primary me-1 my-1">{{ $data->kecamatan->name }}</span>
+                                                        </h6>
+                                                        <span class="fs-12 text-muted">Desa : <span
+                                                                class="badge bg-info me-1 my-1">{{ $data->desa->name }}</span></span>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>{{ $data->jenis_lahan }}</td>
+                                            <td>{{ $data->date }}</td>
+                                            <td class="text-center">
+                                                <a href="{{ url('/pertanian/data_palawija/show/' . $data->id) }}"
+                                                    class="btn btn-primary">
+                                                    <i class="ti ti-eye"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    @foreach (session('filtering') as $filter)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>
+                                                <div class="d-flex contact-image">
+                                                    <div class="d-flex mt-1 flex-column ms-2">
+                                                        <h6 class="mb-0 fs-14 fw-semibold text-dark">Kecamatan : <span
+                                                                class="badge bg-primary me-1 my-1">{{ $filter->kecamatan->name }}</span>
+                                                        </h6>
+                                                        <span class="fs-12 text-muted">Desa : <span
+                                                                class="badge bg-info me-1 my-1">{{ $filter->desa->name }}</span></span>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>{{ $filter->jenis_lahan }}</td>
+                                            <td>{{ $filter->date }}</td>
+                                            <td class="text-center">
+                                                <a href="{{ url('/pertanian/data_palawija/show/' . $filter->id) }}"
+                                                    class="btn btn-primary">
+                                                    <i class="ti ti-eye"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+    <script>
+        $(document).ready(function() {
+            $("#filterKecamatan").change(function() {
+                let filterKecamatan = $("#filterKecamatan").val();
+                $.ajax({
+                    url: "{{ url('/ambil_desa_filtering') }}",
+                    type: "GET",
+                    data: {
+                        kecamatan: filterKecamatan
+                    },
+                    success: function(res) {
+                        $("#filterDesa").html(res);
+                        let selectedDesa = "{{ session('filtering.desa_id') }}";
+                        if (selectedDesa) {
+                            $("#filterDesa").val(selectedDesa).trigger('change');
+                        }
+                    },
+                    error: function(error) {
+                        alert('Gagal mengambil data desa.');
+                    }
+                });
+            });
+
+            let filterKecamatan = $("#filterKecamatan").val();
+            if (filterKecamatan) {
+                $("#filterKecamatan").trigger('change');
+            }
+        });
+    </script>
+@endsection
---
+++ resources/views/pertanian/pages/data/palawija/show.blade.php
@@ -0,0 +1,102 @@
+@extends('index')
+@section('title', 'Detail Data Palawija')
+@section('content')
+    <div class="page-header d-sm-flex d-block">
+        <ol class="breadcrumb mb-sm-0 mb-3">
+            <!-- breadcrumb -->
+            <li class="breadcrumb-item"><a href="{{ url('/pertanian/dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item" aria-current="page">Laporan Palawija</li>
+            <li class="breadcrumb-item active" aria-current="page">Detail Laporan Palawija</li>
+        </ol><!-- End breadcrumb -->
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Laporan Penyuluh</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped" style="width: 100%">
+                            <tr>
+                                <td class="text-right">Tanggal Input</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->date }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Jenis Lahan</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->jenis_lahan }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Nama Penyuluh</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->user_id }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Desa</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->desa->name }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">kecamatan</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->kecamatan->name }}
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="card-header">
+                    <h3 class="card-title">Laporan Lahan</h3>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped" style="width: 100%">
+                            <tr>
+                                <td class="text-right">Jenis Lahan</td>
+                                <td>:</td>
+                                <td>{{ str_replace('_', ' ', $show->jenis_lahan) }}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Jenis Palawija</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->palawija->name }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Jenis Bantuan</td>
+                                <td>:</td>
+                                <td>{{ str_replace('_', ' ', $show->jenis_bantuan) }}</td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Tipe Data input</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->tipe_data }}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-right">Nilai</td>
+                                <td>:</td>
+                                <td>
+                                    {{ $show->nilai }}
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
