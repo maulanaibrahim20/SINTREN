756390cf70203e0f902dee28aee4d0bbdb243547
dianayuan15 <dianayuan15@gmail.com>
2024-06-06 20:01:28 +0700

    conflict

--- database/migrations/2024_03_04_140357_create_tanaman_padis_table.php
+++ database/migrations/2024_03_04_140357_create_tanaman_padis_table.php
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tanaman_padis');
+        Schema::dropIfExists('padis');
     }
 };
