4cfd03d6039418469ed9eecc6eba6cb670673787
GustiAdithiya <g.a.180801@gmail.com>
2024-06-13 10:35:02 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,6 @@ Route::delete('/palawija/deleteDetailById/{id}', [PalawijaController::class, 'de
 Route::get('/admin/getDesa/{id}', [AdminController::class, 'getDesa']);
 Route::get('/padi/showByKecamatan/{id}',[AdminPadiController::class,'showAllByKecamatan']);
 Route::get('/palawija/showByKecamatan/{id}',[AdminPalawijaController::class,'showAllByKecamatan']);
-Route::post('/padi/prediksi',[AdminPadiController::class,'menghitungRegresi']);
 Route::get('/padi/prediksiSp',[AdminPadiController::class,'menghitungRegresiSP']);
 Route::patch('/verify/{id}',[AdminController::class,'verify']);
 Route::get('/admin/getPenyuluh/{id}',[AdminController::class,'getPenyuluh']);
