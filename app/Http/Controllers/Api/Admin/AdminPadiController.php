f0bbb7d7c13b5bb598ffb7960aa905f6d620e1e4
GustiAdithiya <g.a.180801@gmail.com>
2024-06-19 11:40:13 +0700

    update API prediksi

--- app/Http/Controllers/Api/Admin/AdminPadiController.php
+++ app/Http/Controllers/Api/Admin/AdminPadiController.php
@@ -68,128 +68,30 @@ class AdminPadiController extends Controller
         }
     }
 
-    // public function menghitungRegresiSP()
-    // {
-    //     $earliestYear = DB::table('laporan_padis')->orderBy('date', 'ASC')->value(DB::raw('YEAR(date)'));
-    //     $latestYear = DB::table('laporan_padis')->orderBy('date', 'DESC')->value(DB::raw('YEAR(date)'));
-
-    //     $dariTahun = $earliestYear;
-    //     $sampaiTahun = $latestYear;
-
-    //     $laporanPadi = DB::table('laporan_padis')
-    //         ->selectRaw("DATE_FORMAT(date, '%Y-%m') AS bulan")
-    //         ->selectRaw("SUM(CASE WHEN tipe_data = 'panen' THEN nilai ELSE 0 END) AS total_panen")
-    //         ->selectRaw("SUM(CASE WHEN tipe_data = 'tanam' THEN nilai ELSE 0 END) AS total_tanam")
-    //         ->selectRaw("SUM(CASE WHEN tipe_data = 'puso/rusak' THEN nilai ELSE 0 END) AS total_puso_rusak")
-    //         ->whereYear('date', '>=', $dariTahun)
-    //         ->whereYear('date', '<=', $sampaiTahun)
-    //         ->groupBy('bulan')
-    //         ->orderBy('bulan', 'ASC')
-    //         ->get();
-
-    //     $hasilPerTahun = [];
-
-    //     // Mengelompokkan hasil per tahun
-    //     foreach ($laporanPadi as $laporan) {
-    //         $tahun = substr($laporan->bulan, 0, 4);
-
-    //         if (!isset($hasilPerTahun[$tahun])) {
-    //             $hasilPerTahun[$tahun] = 0;
-    //         }
-    //         $hasilPerTahun[$tahun] += ($laporan->total_panen + $laporan->total_tanam - $laporan->total_puso_rusak);
-    //     }
-    //     $actualData = $hasilPerTahun;
-
-    //     $fitur = [];
-    //     $target = [];
-    //     $labels = [];
-    //     foreach ($hasilPerTahun as $tahun => $hasil) {
-    //         $fitur[] = [(int) $tahun];
-    //         $target[] = $hasil;
-    //         $labels[] = $tahun;
-    //     }
-
-    //     $regression = new LeastSquares();
-    //     $regression->train($fitur, $target);
-
-    //     $hasilPrediksi = [];
-    //     $prevValue = null;
-    //     for ($tahun = $dariTahun; $tahun <= $sampaiTahun; $tahun++) {
-    //         // for ($tahun = max(array_keys($hasilPerTahun)) + 1; $tahun <= 2030; $tahun++) {
-    //         $hasilPrediksi[$tahun] = $regression->predict([$tahun]);
-
-    //         if ($tahun > $sampaiTahun) {
-    //             $samples[] = [$tahun];
-    //             $targets[] = $hasilPrediksi[$tahun];
-    //             $labels[] = $tahun;
-    //             $regression->train($samples, $targets);
-    //         }
-
-    //         $change = null;
-    //         if ($prevValue !== null) {
-    //             $change = $hasilPrediksi[$tahun] - $prevValue;
-    //         }
-
-    //         $predictions[] = [
-    //             'year' => $tahun,
-    //             'predicted_value' => $hasilPrediksi[$tahun],
-    //             'change_from_previous_year' => $change
-    //         ];
-
-    //         $prevValue = $hasilPrediksi[$tahun];
-    //     }
-
-    //     $totalError = 0;
-    //     $n = 0;
-    //     foreach ($actualData as $tahun => $aktual) {
-    //         if (isset($hasilPrediksi[$tahun])) {
-    //             $prediksi = $hasilPrediksi[$tahun];
-    //             $totalError += abs(($aktual - $prediksi) / $aktual);
-    //             $n++;
-    //         }
-    //     }
-    //     $mape = round(($totalError / $n) * 100, 2);
-
-    //     $data = [
-    //         'labels' => $labels,
-    //         'actualData' => array_values($actualData),
-    //         'predictedData' => array_column($predictions, 'predicted_value'),
-    //         'mape' => $mape
-    //     ];
-
-    //     return response()->json([
-    //         'status' => 'success',
-    //         'message' => 'Berhasil mendapatkan data',
-    //         'data' => $data
-    //     ], 200);
-    // }
-
-    public function menghitungRegresiSP()
+    public function prediksi()
     {
+        $dariTahun = 2010;
+        $sampaiTahun = 2021;
 
-        $dariTahun = 2013;
-        $sampaiTahun = 2023;
 
         $laporanPadi = DB::table('laporan_padis')
-            ->selectRaw("DATE_FORMAT(date, '%Y-%m') AS bulan")
-            ->selectRaw("SUM(CASE WHEN tipe_data = 'panen' THEN nilai ELSE 0 END) AS total_panen")
-            ->selectRaw("SUM(CASE WHEN tipe_data = 'tanam' THEN nilai ELSE 0 END) AS total_tanam")
-            ->selectRaw("SUM(CASE WHEN tipe_data = 'puso/rusak' THEN nilai ELSE 0 END) AS total_puso_rusak")
+            ->selectRaw('YEAR(date) AS tahun')
+            ->selectRaw('SUM(CASE WHEN tipe_data = "panen" THEN nilai ELSE 0 END) AS total_panen')
             ->whereYear('date', '>=', $dariTahun)
             ->whereYear('date', '<=', $sampaiTahun)
-            ->groupBy('bulan')
-            ->orderBy('bulan', 'ASC')
+            ->groupBy('tahun')
+            ->orderBy('tahun', 'ASC')
             ->get();
 
         $hasilPerTahun = [];
 
         foreach ($laporanPadi as $laporan) {
-            $tahun = substr($laporan->bulan, 0, 4);
+            $tahun = $laporan->tahun;
 
             if (!isset($hasilPerTahun[$tahun])) {
                 $hasilPerTahun[$tahun] = 0;
             }
-            $hasilPerTahun[$tahun] += ($laporan->total_panen + $laporan->total_tanam - $laporan->total_puso_rusak);
+            $hasilPerTahun[$tahun] += $laporan->total_panen;
         }
         $actualData = $hasilPerTahun;
 
@@ -208,7 +110,6 @@ class AdminPadiController extends Controller
         $hasilPrediksi = [];
         $prevValue = null;
         for ($tahun = $dariTahun; $tahun <= $sampaiTahun; $tahun++) {
-            // for ($tahun = max(array_keys($hasilPerTahun)) + 1; $tahun <= 2030; $tahun++) {
             $hasilPrediksi[$tahun] = $regression->predict([$tahun]);
 
             if ($tahun > $sampaiTahun) {
@@ -242,14 +143,6 @@ class AdminPadiController extends Controller
             }
         }
         $mape = round(($totalError / $n) * 100, 2);
-
-
-        // return view('pertanian.pages.prediksi.padiSp.regresiSp', [
-        //     'labels' => $labels,
-        //     'actualData' => array_values($actualData),
-        //     'predictedData' => array_column($predictions, 'predicted_value'),
-        //     'mape' => $mape
-        // ]);
 
         $data = [
             'labels' => $labels,
