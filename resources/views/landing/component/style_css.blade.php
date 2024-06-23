47281a550d4df40214a135b180b9c3fa887f9db2
maulanaibrahim20 <maulibrahim20@student.polindra.ac.id>
2024-06-23 22:31:34 +0700

    update

--- resources/views/landing/component/style_css.blade.php
+++ resources/views/landing/component/style_css.blade.php
@@ -1,6 +1,6 @@
 <!-- Favicons -->
-<link href="{{ url('/landings/img/favicon_io/logo.png') }}" rel="icon">
-<link href="{{ url('/landings/img/favicon_io/apple-touch-icon.png') }}" rel="apple-touch-icon">
+<link href="{{ url('/landings/img/logo-withname-dark.png') }}" rel="icon">
+<link href="{{ url('/landings/img/logo-withname-dark.png') }}" rel="apple-touch-icon">
 
 <!-- Google Fonts -->
 <link
