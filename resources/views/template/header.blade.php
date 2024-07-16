ac23c6f4cde358bcb7f4ed03c42a6e72609809e7
maulanaibrahim20 <maulibrahim20@student.polindra.ac.id>
2024-07-16 12:42:29 +0700

    change image profile

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -216,7 +216,7 @@
                             <div class="dropdown d-flex profile-1">
                                 <a href="javascript:void(0)" data-bs-toggle="dropdown"
                                     class="nav-link leading-none d-flex">
-                                    <img src="{{ url('/assets') }}/images/users/male/user1.png" alt="profile-user"
+                                    <img src="{{ url('/assets') }}/images/user-profile.png" alt="profile-user"
                                         class="avatar  profile-user brround cover-image">
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow" data-bs-popper="none">
